38f49b399d8320de62f447ffa4a20a596d920530
ImHyeLim1209 <75591617+ImHyeLim1209@users.noreply.github.com>
2021-08-29 20:22:00 +0900

    Update middleware.js

--- server/middleware.js
+++ server/middleware.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const router = express.Router();
+
 const middle1 = (req, res, next) => {
   console.log("middle1");
-  next(new Error("middle1 error"));
+  next("route");
 };
 
 const middle2 = (req, res, next) => {
@@ -13,12 +15,18 @@ const middle2 = (req, res, next) => {
 };
 
 const handleError = (err, req, res, next) => {
-  console.log("error!", err);
+  console.log("error");
   next();
 };
 
-app.get("/", middle1, middle2, handleError, (req, res) => {
+app.use("/", router);
+router.get("/", middle1, middle2, (req, res) => {
   console.log("end!");
+  res.json("hello");
+});
+
+router.get("/", (req, res) => {
+  console.log("end2!!");
 });
 
 app.listen(port, () => {
